9debf9b3cdfc5e565bcb5dc5e8c29a28be527080
camkh1 <86780152+camkh1@users.noreply.github.com>
2022-10-04 22:50:00 +0700

    sss

--- youtube/index.php
+++ youtube/index.php
@@ -6,12 +6,15 @@ if (!empty($_SESSION['tokenSessionKey'])) {
     $client = new Google_Client();
     $client->setAccessToken($_SESSION['tokenSessionKey']);
     if($client->isAccessTokenExpired()){
+        echo 11111;
         $isLogin = true;
     }
 } else {
     $isLogin = true;
 }
+echo 2222;
 if(!empty($_COOKIE["email"]) && !empty($isLogin)) {
+    echo 33333;
     $email = $_COOKIE["email"];
     $code = 'code = "";code+= "URL GOTO='.$urlLogin.'\n";code+="TAG POS=1 TYPE=DIV ATTR=TXT:'.$email.'\n";';
     $codeMa = htmlentities($code);
@@ -20,6 +23,7 @@ if(!empty($_COOKIE["email"]) && !empty($isLogin)) {
         header('Location: ' . $urlLogin);
     }
 }
+die;
 if(!empty($_SESSION['back'])) {
     header('Location: ' . $_SESSION['back']);
 }
